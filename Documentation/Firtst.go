9bff842f996f9378fbbfa472f53eefef3836fcdb
Tiago Neves <rykarumstuler@gmail.com>
2021-05-09 20:40:00 -0300

    Fim do módulo básico

--- Documentation/Firtst.go
+++ Documentation/Firtst.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt" // Pacote interno do go de formatação de textos.
+import "fmt"          // Pacote interno do go de formatação de textos.
+import "rsc.io/quote" // Importando pacotes diretamente da internet.
 
 func main() {
-	OlaMundo()
+	//OlaMundo()
+	//QuoteCall()
+	testFmt()
+}
+
+func testFmt() {
+	fmt.Println("Aqui mostra um texto normal")
 }
 
 func OlaMundo() {
 	fmt.Println("Eae mundão!!!")
+}
+
+func QuoteCall() {
+	fmt.Println("Go:", quote.Go())         // Funciona perfeitamente e adiciona um espaço automático.
+	fmt.Println("Glass: " + quote.Glass()) // Apenas concatena.
+	fmt.Println("Hello: " + quote.Hello())
+	fmt.Println("Opt:", quote.Opt()) // Que frase em parça
 }
